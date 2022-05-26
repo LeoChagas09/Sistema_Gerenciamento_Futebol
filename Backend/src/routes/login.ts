06adbc4c4540e4dd6cbb454b4cd63d9920ffb152
LeoChagas09 <LeoChagas09@hotmail.com>
2022-05-26 18:04:22 -0300

    Buscando no banco de dados se o usuario existe
    Uma rota para buscar se o usuario que irá logar está cadastrado

--- Backend/src/routes/login.ts
+++ Backend/src/routes/login.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express';
+import { prisma } from '../prisma';
 
 export const LoginRoutes = Router();
 
-LoginRoutes.post('/', (req, res) => {
-  console.log(req.body);
-  res.json({ message: 'Login' });
+LoginRoutes.post('/', async (req, res) => {
+  const { login, senha } = req.body;
+
+  const usuario = await prisma.usuarios.findMany({
+    where: {
+      email: login,
+      senha,
+    },
+  });
+
+  console.log(usuario);
+
+  res.status(200).json('Usuario logado com sucesso');
 });
