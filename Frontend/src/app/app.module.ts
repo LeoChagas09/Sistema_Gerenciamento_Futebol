298055a8315a930b164466f8b679e43c7f3a1d27
LeoChagas09 <LeoChagas09@hotmail.com>
2022-06-02 09:23:04 -0300

    Criando o cabeçalho do site
    Fazendo o cabeçalho da página

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CriarContaComponent } from './components/criar-conta/criar-conta.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CriarContaComponent
+    CriarContaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
